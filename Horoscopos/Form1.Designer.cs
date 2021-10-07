9a5c711dfb6fe5420f37939fdc07326ffd6ee32e
angelvalenciamorales <21340736@itnogales.edu.mx>
2021-10-07 11:31:06 +0200

    El final del modelo para resolver el algoritmo

---
+++ Horoscopos/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace Horoscopos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboListMes = new System.Windows.Forms.ComboBox();
+            this.cboListDia = new System.Windows.Forms.ComboBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.lblMostrar = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.cboListMes);
+            this.panel1.Controls.Add(this.cboListDia);
+            this.panel1.Controls.Add(this.btnMostrar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(318, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(34, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Mes";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(34, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Dia";
+            // 
+            // cboListMes
+            // 
+            this.cboListMes.FormattingEnabled = true;
+            this.cboListMes.Items.AddRange(new object[] {
+            "Enero",
+            "Febrero",
+            "Marzo",
+            "Abril",
+            "Mayo",
+            "Junio",
+            "Julio",
+            "Agosto",
+            "Septiembre",
+            "Octubre ",
+            "Noviembre",
+            "Diciembre"});
+            this.cboListMes.Location = new System.Drawing.Point(103, 160);
+            this.cboListMes.Name = "cboListMes";
+            this.cboListMes.Size = new System.Drawing.Size(182, 24);
+            this.cboListMes.TabIndex = 3;
+            // 
+            // cboListDia
+            // 
+            this.cboListDia.FormattingEnabled = true;
+            this.cboListDia.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30"});
+            this.cboListDia.Location = new System.Drawing.Point(103, 80);
+            this.cboListDia.Name = "cboListDia";
+            this.cboListDia.Size = new System.Drawing.Size(182, 24);
+            this.cboListDia.TabIndex = 2;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.BackColor = System.Drawing.Color.Maroon;
+            this.btnMostrar.FlatAppearance.BorderSize = 0;
+            this.btnMostrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkRed;
+            this.btnMostrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnMostrar.Location = new System.Drawing.Point(98, 328);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(187, 29);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Moatrar";
+            this.btnMostrar.UseVisualStyleBackColor = false;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // lblMostrar
+            // 
+            this.lblMostrar.AutoSize = true;
+            this.lblMostrar.BackColor = System.Drawing.Color.Transparent;
+            this.lblMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMostrar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblMostrar.Location = new System.Drawing.Point(491, 196);
+            this.lblMostrar.Name = "lblMostrar";
+            this.lblMostrar.Size = new System.Drawing.Size(79, 29);
+            this.lblMostrar.TabIndex = 1;
+            this.lblMostrar.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMostrar);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboListMes;
+        private System.Windows.Forms.ComboBox cboListDia;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label lblMostrar;
+    }
+}
+
---
+++ Horoscopos/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Horoscopos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /*-----------Variables------------*/
+        int mes, dia;
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+
+            dia = Convert.ToInt32(cboListDia.Text);
+            mes = cboListMes.SelectedIndex + 1;
+            lblMostrar.Text = "";
+
+            //if ((dia >= 20 && dia <= 31 && mes == 1) || (dia >= 1 && dia <= 18 && mes == 2 )) 
+            //Enero 20 – Febrero 18
+
+            switch (mes)
+            {
+                //Acuario
+                case 1:
+                    if (dia >= 20 && dia <= 31 && mes == 1)
+                    {
+                        lblMostrar.Text = "Acuario";
+                    }
+                    break;
+
+                case 2:
+                    //Acuario
+                     if (dia >= 1 && dia <= 18 && mes == 2)
+                    {
+                        lblMostrar.Text = "Acuario";
+                    }
+
+                     //pucisis
+                     if(dia >=19 && dia <= 31 && mes == 2)
+                    {
+                        lblMostrar.Text = "Piscis";
+                    }
+                    break; // error de pocision 
+                    
+                case 3:
+                    //pucsis
+                    if(dia >= 21 && dia <= 31 && mes == 3){
+                        lblMostrar.Text = "Aries";
+                    }
+
+                    if (dia <= 30 && dia <= 20 && mes == 3)
+                    {
+                        lblMostrar.Text = "Piscis";
+                    }
+                    break; // error de pocision 
+
+                    //Aries
+
+
+
+
+
+
+            }
+        }
+    }
+}
